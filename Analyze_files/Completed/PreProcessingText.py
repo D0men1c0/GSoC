4c106e1f18bcf34b608a24dc802dec45060e6d7f
D0men1c0 <82759768+D0men1c0@users.noreply.github.com>
2024-06-12 02:26:42 +0200

    Update Function to Extract Text

--- Analyze_files/Completed/PreProcessingText.py
+++ Analyze_files/Completed/PreProcessingText.py
@@ -1,11 +1,13 @@
 import nltk
+import re
+from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-
 nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
+
 
 def preprocess_title(title):
     """
@@ -34,3 +36,32 @@ def preprocess_content(content):
     tokens = [token.lower() for token in tokens if token.isalpha()]
     tokens = [token for token in tokens if token not in stopwords.words('english')]
     return ' '.join(tokens)
+
+
+def clean_sentences(text):
+    """
+    Cleans a sentence by removing multiple spaces, punctuation, non-alphanumeric characters, leading and trailing spaces,
+    numbers, and remaining underscores.
+    :param text: the sentence to clean
+    :return: the cleaned sentence
+    """
+    if '<' in text and '>' in text:
+        # Remove HTML tags
+        text = BeautifulSoup(text, "html.parser").get_text()
+    # Remove non-alphanumeric characters (excluding spaces and numbers)
+    text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+    # Remove multiple spaces
+    text = re.sub(r'\s+', ' ', text)
+    # Remove leading and trailing spaces
+    text = text.strip()
+    # Remove single characters (if desired)
+    text = re.sub(r'\b\w\b', '', text)
+    # Remove any remaining underscores (if needed)
+    text = text.replace('_', '')
+    # Remove punctuation
+    text = re.sub(r'[^\w\s]', '', text)
+    # Lowercase the text
+    text = text.lower()
+    # Remove multiple spaces
+    text = re.sub(r'\s+', ' ', text)
+    return text
